285c682a9eb7d83f6999d6954e9490a7f1853250
cutthroat <cutthroat78@tutanota.com>
2022-06-20 02:04:34 +0100

    Added id and audio file field

--- main.py
+++ main.py
@@ -3,11 +3,13 @@ from bs4 import BeautifulSoup
 
 URL = "https://oneidalanguage.ca/learn-our-language/oneidalanguage-words-phrases/page/"
 
+ID = 0
+
 if not os.path.exists("./audio"):
     os.makedirs("./audio")
     
-file = open("Oneida-Words-and-Phrases.csv", "a")
-file.write("Oneida,English\n") # Heading at to say which side is Oneida and which side is English
+file = open("Oneida-Canada-Words-and-Phrases.csv", "a")
+file.write("ID,Oneida,English,Audio File\n") # Heading at to say which side is Oneida and which side is English
 
 for page in range(1, 93):
     req = requests.get(URL + str(page) + '/')
@@ -21,11 +23,12 @@ for page in range(1, 93):
       
     # Make CSV and append for page
     for one,eng,url in zip(oneida_word,english_word,audio_links):
-      file.write(str(one.get_text()) + "," + str(eng.get_text()) + "\n")
+      ID += 1
+      file.write(str(ID) + "," + "\"" + str(one.get_text()) + "\"" + "," + "\"" + str(eng.get_text()) + "\"" + "," + "\"" + "./audio/" + str(ID) + ".mp3" + "\"" + "\n")
       r = requests.get(url)
-      filename = one.get_text() + ".mp3"
+      filename = str(ID) + ".mp3"
       with open(filename, 'wb') as f:
         f.write(r.content)
-      os.rename("./" + str(one.get_text()) + ".mp3", "./audio/" + str(one.get_text()) + ".mp3")
+      os.rename("./" + str(ID) + ".mp3", "./audio/" + str(ID) + ".mp3")
       
 file.close()
